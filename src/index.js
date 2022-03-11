53a2a27cf1d640ce61eff5a368f6d31c86ab995f
MrSunfat <trannhatdanh15062001@gmail.com>
2022-03-11 18:57:55 +0700

    Refactor Folder MVC

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 8080;
 
-app.use(express.static(`${path.join(__dirname)}\\public`))
+const route = require('./routes');
+
+app.use(express.static(`${path.join(__dirname)}\\public`));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -16,11 +25,9 @@ app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', `${path.join(__dirname)}\\resources\\views`);
 
-// console.log(`${path.join(__dirname)}\\resources\\views`);
+// Routes init
+route(app);
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
